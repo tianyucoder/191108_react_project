5c2b37d045a5d5fcabd0d44c99eb741e06ff8ec9
tianyu5177 <zhangtianyu5177@sina.com>
2020-03-31 16:54:41 +0800

    更新

--- src/containers/admin/header/header.jsx
+++ src/containers/admin/header/header.jsx
@@ -1,18 +1,56 @@
 import React, {Component} from 'react'
-import {Button} from 'antd'
-import {FullscreenOutlined} from '@ant-design/icons';
+import {Button,Modal} from 'antd'
+import {connect} from 'react-redux'
+import {
+	FullscreenOutlined,
+	FullscreenExitOutlined,
+	ExclamationCircleOutlined
+} from '@ant-design/icons';
+import screenfull from 'screenfull'
+import {createDeleteUserAction} from '../../../redux/actions/login'
 import './css/header.less'
 
-export default class Header extends Component {
+const {confirm} = Modal;
+
+class Header extends Component {
+
+	state = {
+		isFull:false //标识是否全屏
+	}
+
+	fullScreen = ()=>{
+		screenfull.toggle();
+	}
+
+	logout = ()=>{
+		confirm({
+			title: '确定退出登录吗？', //弹窗主标题
+			//content: '若退出登录，需要重新登录', //弹窗的副标题
+			icon: <ExclamationCircleOutlined />, //弹窗中展示的图标
+			okText:'确认',
+			cancelText:'取消',
+			onOk:()=> { //确认按钮的回调
+				this.props.deleteUser()
+			}
+		});
+	}
+
+	componentDidMount(){
+		screenfull.onchange(()=>{
+			let {isFull} = this.state
+			this.setState({isFull:!isFull})
+		})
+	}
+
 	render() {
 		return (
 			<div className="header-wraper">
 				<div className="header-top">
-					<Button size="small">
-						<FullscreenOutlined/>
+					<Button onClick={this.fullScreen} size="small">
+						{this.state.isFull?	<FullscreenExitOutlined/> : <FullscreenOutlined/>}
 					</Button>
-					<span className="user-show">欢迎，xxxx</span>
-					<Button type="link">退出登录</Button>
+					<span className="user-show">欢迎，{this.props.username}</span>
+					<Button onClick={this.logout} type="link">退出登录</Button>
 				</div>
 				<div className="header-bottom">
 					<div className="bottom-left">
@@ -28,3 +66,10 @@ export default class Header extends Component {
 		)
 	}
 }
+
+export default connect(
+	(state)=>({username:state.userInfo.user.username}),//传递状态
+	{
+		deleteUser:createDeleteUserAction
+	}//传递操作状态的方法
+)(Header)
