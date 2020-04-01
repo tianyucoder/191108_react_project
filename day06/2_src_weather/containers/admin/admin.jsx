d3f9d896cf65a5044ff7f8c7ded7f06b90eb5ad9
tianyu5177 <zhangtianyu5177@sina.com>
2020-04-01 10:56:07 +0800

    更新

---
+++ day06/2_src_weather/containers/admin/admin.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Layout } from 'antd';
+import {Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
+import Header from './header/header'
+import './css/admin.less'
+
+const {Footer, Sider, Content } = Layout;
+
+class Admin extends Component {
+	render() {
+		if(!this.props.isLogin) return <Redirect to="/login"/>
+		return (
+			<Layout className="admin-wraper">
+				<Sider>Sider</Sider>
+				<Layout>
+					<Header/>
+					<Content>Content</Content>
+					<Footer>Footer</Footer>
+				</Layout>
+			</Layout>
+		)
+	}
+}
+
+export default connect(
+	(state)=>({
+		isLogin:state.userInfo.isLogin
+	}),
+	{}
+)(Admin)
